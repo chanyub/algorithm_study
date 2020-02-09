2d374f0647a7ea619031acb1e9b81bcac386f852
chanyub <chanyub1008@naver.com>
2020-02-09 23:51:45 +0900

    백준 2156

--- 2020 02 08.py
+++ 2020 02 08.py
@@ -1,3 +1,11 @@
-R1,S = map(int,input().split())
-R2 = 2*S - R1
-print(R2)
+N,X = map(int,input().split())
+A = list(map(int,input().split()))
+B = []
+
+for x in range (N) :
+    if A[x] < X :
+        B.append(A[x])
+
+
+for x in range (len(B)) :
+    print(B[x],end=' ')
--- 2020 02 09.py
+++ 2020 02 09.py
@@ -1,11 +1,19 @@
-N,X = map(int,input().split())
-A = list(map(int,input().split()))
-B = []
+n = int(input())
 
-for x in range (N) :
-    if A[x] < X :
-        B.append(A[x])
+A = [0 for x in range (n+1)]
 
+for x in range (1,n+1) :
+    A[x] = int(input())
 
-for x in range (len(B)) :
-    print(B[x],end=' ')
+DP = [0 for x in range (n+1)]
+
+for i in range (n+1) :
+    if i == 1 :
+        DP[i] = A[i]
+    elif i == 2 :
+        DP[i] = A[i] + A[i-1]
+    elif i == 3 :
+        DP[i] = max(A[i] + A[i-1], A[i-1] + A[i-2], A[i-2] + A[i])
+    elif i >= 4 :
+        DP[i] = max(A[i-1] + DP[i-3] + A[i], DP[i-2] + A[i],A[i-1] + DP[i-3], DP[i-2],DP[i-1])
+print(max(DP))
